cb318e10fd9b40672f18311ffdf6244ec74dd93a
denislavninov <deniztopcuoglu@gmail.com>
2025-03-19 22:52:49 +0100

    Add new images for profile and studio sections, enhance Navbar with improved layout and styling, and update Services and Studio pages with new content and functionality

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import Link from 'next/link';
@@ -11,11 +11,14 @@ const Navbar = ({ contactRef }: { contactRef: React.RefObject<HTMLDivElement | n
   const isHomePage = pathname === '/';
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
+
   const scrollToContact = () => {
     if (contactRef && contactRef.current) {
       contactRef.current.scrollIntoView({ behavior: 'smooth' });
     }
   };
+
+
 
   return (
     <div
@@ -27,7 +30,7 @@ const Navbar = ({ contactRef }: { contactRef: React.RefObject<HTMLDivElement | n
           <Image src="/painter1.avif" alt="painter1" fill style={{ objectFit: 'cover' }} />
         </div>
       )}
-      <nav className="absolute top-0 left-0 right-0 p-4 flex items-center h-[100px] z-10 md:justify-between justify-start">
+      <nav className="absolute top-0 left-0 right-0 p-4 flex items-center h-[100px]  z-10 md:justify-evenly justify-start">
         <div className="flex items-center">
           <Image src="/logo.avif" alt="logo" width={40} height={40} className="rounded-full" onClick={() => router.push('/')} />
         </div>
@@ -37,22 +40,20 @@ const Navbar = ({ contactRef }: { contactRef: React.RefObject<HTMLDivElement | n
           </button>
         </div>
         <ul className={`flex-col md:flex-row md:flex space-x-8 ${isMenuOpen ? 'flex' : 'hidden'} md:space-x-8 mt-8 md:mt-0`} style={{ color: 'var(--foreground)' }}>
-          <Link href="/about-us" className="font-bold">ABOUT US</Link>
-          <Link href="/projects" className="font-bold">PROJECTS</Link>
-          <Link href="/studio" className="font-bold">STUDIO</Link>
-          <Link href="/services" className="font-bold">SERVICES</Link>
-          <Link href="/gallery" className="font-bold">GALLERY</Link>
+          <Link href="/about-us" className="font-bold text-xl">ABOUT US</Link>
+          <Link href="/projects" className="font-bold text-xl">PROJECTS</Link>
+          <Link href="/studio" className="font-bold text-xl">STUDIO</Link>
+          <Link href="/services" className="font-bold text-xl">SERVICES</Link>
         </ul>
-        <button className="hidden md:block bg-white hover:bg-black hover:text-white text-gray-800 px-4 py-2 rounded-xs" onClick={scrollToContact}>GET IN TOUCH</button>
+        <button className="hidden md:block bg-white hover:bg-black hover:text-white text-gray-800 text-md px-4 py-2 rounded-xs" onClick={scrollToContact}>GET IN TOUCH</button>
       </nav>
-      <div className="absolute top-80 ml-10 z-10" style={{ color: 'var(--foreground)' }}>
+      <div className="absolute top-100 ml-10 z-10" style={{ color: 'var(--foreground)' }}>
         {isHomePage && (
           <>
-            <h1 className="text-5xl font-bold">Medium length hero heading goes here</h1>
-            <p className="mt-4 text-lg">Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br /> Suspendisse varius enim in eros elementum tristique.</p>
+            <h1 className="text-5xl font-bold">The Art of Painting</h1>
+            <p className="mt-4 text-xl font-bold">We are a team of artists who are passionate about creating beautiful art.</p>
             <div className="mt-4">
-              <button className="bg-white hover:bg-black hover:text-white text-gray-800 px-4 py-2 rounded-xs mr-2">Button</button>
-              <button className="bg-white hover:bg-black hover:text-white text-gray-800 px-4 py-2 rounded-xs">Button</button>
+              <button className="bg-white hover:bg-black hover:text-white text-gray-800 text-lg px-4 py-2 rounded-xs mr-2">Scroll Down  </button>
             </div>
           </>
         )}
--- src/app/components/Slider.tsx
+++ src/app/components/Slider.tsx
@@ -5,8 +5,9 @@ import eskirumeli from '../../../public/eskirumeli.avif';
 import yenirumeli from '../../../public/yenirumeli.avif';
 
 const Slider = () => {
+
   return (
-    <div className="flex justify-center items-center py-10">
+    <div className="flex justify-center items-center py-10" >
       <div className="shadow-lg rounded-lg p-6 w-full max-w-[1200px] md:h-[600px] sm:h-[400px] h-auto overflow-hidden relative flex flex-col items-center justify-center" style={{ backgroundColor: 'var(--background)', color: 'var(--foreground)' }}>
         <div className="w-full h-auto md:h-[500px] overflow-hidden relative flex items-center justify-center">
           <Compare

67c92cbcdb92300d18b0a5a6b54d558f4c325387
denislavninov <deniztopcuoglu@gmail.com>
2025-03-19 12:38:19 +0100

    Add About Us section with new image and improved layout, and enhance Projects page with project listings and responsive design

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -1,26 +1,34 @@
 'use client'
 import React from 'react';
 import { useRouter } from 'next/navigation';
+import Image from 'next/image';
 
 const AboutUs = () => {
   const router = useRouter();
   return (
-    <div className="">
-      <div className="max-w-4xl mx-auto px-4 py-16 mt-50 mb-20 bg-gradient-to-r from-slate-500 to-slate-700  shadow-2xl rounded-lg ">
-
+    <div className="flex flex-col items-center justify-center mt-20">
+      <div className="max-w-4xl mx-auto px-4 py-16 mt-10 mb-20">
+        <h1 className="text-4xl font-bold text-center mb-8">About Us</h1>
         <p className="text-lg text-left mb-4 leading-relaxed ">
           Welcome to our company! We are dedicated to providing the best services in the industry.
           Our team of experts is committed to delivering high-quality solutions tailored to your needs.
         </p>
-        <p className="text-lg text-left mb-4 leading-relaxed">
+        <Image src="/aboutus.avif" alt="aboutus" width={600} height={400} className="rounded-lg shadow-xl w-full h-auto mb-10" />
+        <p className="text-lg text-left mb-4 leading-relaxed ">
           Our mission is to innovate and lead in our field, ensuring customer satisfaction and excellence in every project.
           We believe in the power of collaboration and strive to build lasting relationships with our clients.
         </p>
         <p className="text-lg text-left mb-4 leading-relaxed">
           Thank you for choosing us. We look forward to working with you and achieving great success together.
         </p>
+
+      </div >
+      <div className="flex justify-center mt-8">
+        <button className=" text-white px-4 py-2 rounded-xs cursor-pointer" onClick={() => router.push('/')}>
+          Back to Home
+        </button>
       </div>
-    </div>
+    </div >
   );
 };
 

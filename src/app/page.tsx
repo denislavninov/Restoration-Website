e568bb2bda317569fd53ca6860b4071c3c84cdb7
denislavninov <deniztopcuoglu@gmail.com>
2025-03-20 13:50:08 +0100

    Add Slider component to Home page for enhanced user interaction

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 "use client"
 import Gallery from "./components/Gallery";
-
+import Slider from "./components/Slider";
 export default function Home() {
   return (
     <main>
+      <Slider />
       <Gallery />
+
     </main>
   );
 }

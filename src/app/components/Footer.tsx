bb90f8cbae934dad94559777a3661878c2a1ddde
denislavninov <deniztopcuoglu@gmail.com>
2025-03-20 13:56:14 +0100

    Update Footer component with new contact information and social media links for better user engagement

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -10,14 +10,14 @@ const Footer = () => {
 
           <div className="mb-4 md:mb-0">
             <h2 className="text-xl font-bold text-center">Contact Us</h2>
-            <p className="text-sm">Email: info@example.com</p>
-            <p className="text-sm">Phone: +123 456 7890</p>
+            <p className="text-sm">Email: altugkatmer@gmail.com</p>
+            <p className="text-sm">Phone: +90 506 606 94 10</p>
           </div>
           <div className="flex space-x-6">
-            <a href="#" className="text-white hover:text-gray-400 text-2xl"><SlSocialInstagram /></a>
-            <a href="#" className="text-white hover:text-gray-400 text-2xl"><SlSocialFacebook /></a>
+            <a href="https://www.instagram.com/altugkatmer?igsh=MWg3aWV5YXJubGhmYg==" className="text-white hover:text-gray-400 text-2xl"><SlSocialInstagram /></a>
+            <a href="https://www.facebook.com/tablorestorasyonu.altugkatmer/?locale=tr_TR" className="text-white hover:text-gray-400 text-2xl"><SlSocialFacebook /></a>
             <a href="#" className="text-white hover:text-gray-400 text-2xl"><SlSocialTwitter /></a>
-            <a href="#" className="text-white hover:text-gray-400 text-2xl"><SlSocialLinkedin /></a>
+            <a href="https://www.linkedin.com/in/altu%C4%9F-katmer-273a15167?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app" className="text-white hover:text-gray-400 text-2xl"><SlSocialLinkedin /></a>
           </div>
         </div>
         <div className="mt-auto text-center pt-6">

96274aa57719492bbfed9a378e4126b1e1a774de
denislavninov <deniztopcuoglu@gmail.com>
2025-03-20 13:45:14 +0100

    Remove unused imports from the Home component to streamline the code and improve readability.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,4 @@
 "use client"
-import Image from "next/image";
-import Navbar from "./components/Navbar";
-import CtaSection from "./components/CtaSection";
-import Footer from "./components/Footer";
 import Gallery from "./components/Gallery";
 
 export default function Home() {
